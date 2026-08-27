b97cfeb6cd709c2a8861d84c77a5e447dac23977
Sai Tej Sunkara <saitejsunka@gmail.com>
2026-08-26 20:11:10 -0700

    infra: updated database static ip address to secret manager so that application can access it easily without hardcoding

--- infra/components/secrets.tf
+++ infra/components/secrets.tf
@@ -35,3 +35,20 @@ resource "google_secret_manager_secret_version" "db_password_version" {
   secret      = google_secret_manager_secret.db_password.id
   secret_data = var.db_password
 }
+
+# 3. Database Primary Private IP Secret
+# Dynamically stores the auto-generated private IP so the application can fetch it at runtime
+resource "google_secret_manager_secret" "db_ip" {
+  secret_id = "expo-db-ip"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [google_project_service.secretmanager]
+}
+
+resource "google_secret_manager_secret_version" "db_ip_version" {
+  secret      = google_secret_manager_secret.db_ip.id
+  secret_data = google_sql_database_instance.primary.private_ip_address
+}
